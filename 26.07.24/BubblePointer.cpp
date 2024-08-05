bf57c0af1a7e6b27b0f3dbbaf47fb66db37a1e5a
Sonal Singh <80062294+sonalsinghk@users.noreply.github.com>
2024-08-05 15:58:04 +0530

    Update BubblePointer.cpp

--- 26.07.24/BubblePointer.cpp
+++ 26.07.24/BubblePointer.cpp
@@ -1,3 +1,33 @@
+One way:
+#include<iostream>
+using namespace std;
+void bubbleSort(int *a, int n){
+     
+     for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }
+     }
+}
+void printArray(int *a, int n){
+    for(int i=0;i<n;i++){
+        cout<<*(a+i);
+    }
+}
+int main(){
+    int a[] = {1,4,3,6,5};
+    int n = sizeof(a)/sizeof(int);
+
+    bubbleSort(a,n);
+    printArray(a,n);
+
+    return 0;
+}
+
+Second Way:
+
 #include<iostream>
 using namespace std;
 void bubbleSort(int *a, int n){
